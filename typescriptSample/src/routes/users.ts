591f54ac3080a0ace5bf38fc532f7873f05ffbf0
jkronyak <56709613+jkronyak@users.noreply.github.com>
2022-04-21 23:30:41 -0400

    basic routes and data functions for users collection

---
+++ typescriptSample/src/routes/users.ts
@@ -0,0 +1,89 @@
+import express = require('express');
+const router: express.Router = new (express.Router as any)();
+import data from '../data';
+import * as utils from '../utils';
+const users = data.users;
+
+// Note: these routes do not yet consider any form of authentication.
+// They merely exist to start us off.
+
+router.get('/', async (req, res) => {
+  try {
+    const foundUsers = await users.getAll();
+    res.json(foundUsers);
+  } catch (e) {
+    console.log(e);
+    res.status(404).json({ error: e });
+  }
+});
+
+router.post('/signup', async (req, res) => {
+  try {
+    const b = req.body;
+    console.log(
+      b.email,
+      b.password,
+      b.firstName,
+      b.lastName,
+      b.appointmentIds,
+      b.reviewIds,
+      b.level
+    );
+    const usr = utils.validateUser(
+      b.email,
+      b.password,
+      b.firstName,
+      b.lastName,
+      b.appointmentIds,
+      b.reviewIds,
+      b.level
+    );
+    res.json(await users.create(usr));
+  } catch (e) {
+    console.log(e);
+    res.status(404).json({ error: e });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const _id = utils.checkId(req.params.id, 'user');
+    const usr = await users.getById(_id);
+    res.json(usr);
+  } catch (e) {
+    console.log(e);
+    res.status(404).json({ error: e });
+  }
+});
+// router.post('/login', async (req, res) => {
+// 	try {
+// 		const b = req.body;
+// 		console.log(b.email, b.password, b.firstName, b.lastName,
+// 			b.appointmentIds, b.reviewIds, b.level);
+
+// 		const usr = utils.validateUser(
+// 			b.email,
+// 			b.password,
+// 			b.firstName,
+// 			b.lastName,
+// 			b.appointmentIds,
+// 			b.reviewIds,
+// 			b.level
+// 		);
+// 		const checkUser = await users.checkUser(usr);
+// 		if(checkUser) { 		// If the user already exists
+// 			res.json(checkUser);
+// 		}
+
+// 	} catch (e) {
+// 		console.log(e);
+// 		res.status(404).json({error: e});
+// 	}
+// });
+
+// router.get('/logout', async(req, res) => {
+// 	// req.session.destroy();
+// 	res.json({message: "you logged out son"});
+// });
+
+export = router;
