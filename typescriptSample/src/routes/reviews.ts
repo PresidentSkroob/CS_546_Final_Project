6682ab1604409245a1e6f87e4cafd608d5158abd
jkronyak <56709613+jkronyak@users.noreply.github.com>
2022-04-28 13:08:46 -0400

    basic data rendering for the '/' reviews page

--- typescriptSample/src/routes/reviews.ts
+++ typescriptSample/src/routes/reviews.ts
@@ -3,11 +3,28 @@ const router: express.Router = new (express.Router as any)();
 import data from '../data';
 import * as utils from '../utils';
 const reviews = data.reviews;
+const users = data.users;
 
 router.get('/', async (req, res) => {
   try {
     const foundReviews = await reviews.getAll();
-    res.json(foundReviews);
+	let relevantInformation = [];
+
+	for(let i = 0; i < foundReviews.length; i++) {
+		let foundCustomer = await users.getById(foundReviews[i].posterId);
+		let foundHairdresser = await users.getById(foundReviews[i].hairdresserId);
+		let customerName = foundCustomer.firstName + " " +foundCustomer.lastName;
+		let hairdresserName = foundHairdresser.firstName + " " + foundHairdresser.lastName;
+		let cur = {
+			customerName: customerName,
+			hairdresserName: hairdresserName,
+			rating: foundReviews[i].rating,
+			body: foundReviews[i].body
+		};
+		relevantInformation.push(cur);
+	}
+	res.render('reviews', { reviews: relevantInformation });
+    // res.json(foundReviews);
   } catch (e) {
     console.log(e);
     res.status(404).json({ error: e });
