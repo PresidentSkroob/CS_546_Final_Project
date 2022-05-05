555b44a3d335335e1a96ed5782f6f7a70a3c065e
Eric <esong@stevens.edu>
2022-05-05 17:44:07 -0400

    alice's qr code implementation

--- typescriptSample/src/routes/contact.ts
+++ typescriptSample/src/routes/contact.ts
@@ -1,5 +1,16 @@
 import express = require('express');
 const router: express.Router = new (express.Router as any)();
+import qrcode from 'qrcode';
+
+
+router.route('/qr').post(async (req, res) => {
+  try {
+    const im = await qrcode.toDataURL(req.body.url)
+    return res.json({ success: true, img: im });
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+});
 
 router.use('/', async (req, res) => {
   res.render('contact', { title: 'Contact Us' });
@@ -8,4 +19,5 @@ router.use('/', async (req, res) => {
 router.use('*', (_, res) => {
   res.status(404).json({ error: 'Not found' });
 });
+
 export = router;
