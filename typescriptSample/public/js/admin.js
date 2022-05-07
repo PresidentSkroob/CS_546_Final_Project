50eecd332014ad73a9e2a0e6f596d91f69e895eb
ahuston-0 <aliceghuston@gmail.com>
2022-05-07 16:23:25 -0400

    Adds admin js/css
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

---
+++ typescriptSample/public/js/admin.js
@@ -0,0 +1,26 @@
+$(function () {
+  $('.levelDrop').change((e) => {
+    const id = $(e.currentTarget).parent().attr('id');
+    const lvl = $(e.currentTarget).find('option:selected').val();
+    console.log(lvl);
+    const requestConfig = {
+      method: 'PATCH',
+      url: `/users/${id}`,
+      data: { level: lvl },
+    };
+    $.ajax(requestConfig).then(
+      (response) => {
+        if (response.error) {
+          err(response.error);
+        }
+      },
+      (response) => {
+        err(response.responseJSON.error);
+      }
+    );
+  });
+
+  $('h3').click((e) => {
+    $(e.currentTarget).siblings('div').eq(0).toggle();
+  });
+});
